4cc6ee901792640942019ba30bb3ea73f42ae078
oath <aryajati45@gmail.com>
2025-11-03 19:13:35 +0700

    tes tag versi docker ke dockerhub

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Halo, ini adalah proyek DevOps pertama saya!' \
-    ' tes 1'
+    ' tes 1' \
+    ' tes tag'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
